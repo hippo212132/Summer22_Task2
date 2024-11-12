36ee66bed8610dc0814a2e9bcc00903b9ae3f403
Your Name <you@example.com>
2024-11-12 12:29:10 +0000

    database

--- app.py
+++ app.py
@@ -1,27 +1,62 @@
 from flask import Flask, render_template, request, redirect, url_for, sessions
-import db
+
+
+
+from cs50 import SQL
+
+import hashlib
+
+db = SQL("sqlite:///C:/Users/337909/OneDrive - Milton Keynes College O365/Task 1/Flask/database.db")
 
 app = Flask(__name__)
 
-
+def hash(phrase): #This is a function used to hash a new password
+ 
+    object = hashlib.md5(phrase.encode())
+    object = object.hexdigest()
+ 
+    return object
 
 
 @app.route("/")
-def signup():
+def index():
     return render_template("index.html")
-def home():
+
+@app.route("/login")
+def login():
     return render_template("login.html")
+
+
 
 @app.route("/signup", methods = ["GET", "POST"])
 def home():
-    if request.method == "GET":
-        return render_template("signup.html")
-    else:
-        email = request.form.get("email")
-        name = request.form.get("user")
-        password = request.form.get("pass")
-        db.signup(email, name, password)
-        return redirect("/")
+    if request.method == "POST":
+        #Checking is method is POST or Get
+        action = request.form['action']
+        #When the Button on HTML form is pressed
+        if action == 'register':
+            #When the button value == register
+            email = request.form.get("email")
+            name = request.form.get("name")
+            password = request.form.get("password")
+            #gathers information submitted via the form
+            if email == '' or name == '' or password == '':
+                return redirect("/signup")
+            #error handling - if each box = nothing will refresh page.
+            
+            else:
+
+                hash(password)
+                #hashing
+
+                db.execute("INSERT INTO users (email, name, password) VALUES (?)", (email, name, password))
+                #inserts form inputted values into the database with hashes password
+                return redirect("/login")
+                #sends to login once inserted into database
+            
+    return render_template("signup.html")
+
+##redirect is used when a @route is made
 
 
 if __name__ == "__main__":
--- db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 
-
-db = sqlite3.connect("./database.db", check_same_thread=False)
-
-
-
-def signup(email, user, password):
-    db.execute(f"INSERT INTO users (email, name,pass) VALUES ('{email}','{user}', '{password}')")
-    db.commit()
